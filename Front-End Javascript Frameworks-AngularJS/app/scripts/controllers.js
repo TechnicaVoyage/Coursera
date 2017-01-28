8bd150301e61024726095bbc4ba45121289efdda
TechnicaVoyage <dhavalat60@gmail.com>
2017-01-29 02:25:12 +0530

    Completed Excercise: Client-Server Communication using $http

--- Front-End Javascript Frameworks-AngularJS/app/scripts/controllers.js
+++ Front-End Javascript Frameworks-AngularJS/app/scripts/controllers.js
@@ -3,8 +3,11 @@ angular.module('confusionApp').controller('MenuController',['$scope','menuFactor
   $scope.tab = 1;
   $scope.filtText = '';
 
-  $scope.dishes= menuFactory.getDishes();
+  $scope.dishes= [];
 
+  menuFactory.getDishes().then( function(response) {
+    $scope.dishes = response.data;
+  });
   $scope.select = function(setTab) {
     $scope.tab = setTab;
 
@@ -65,8 +68,11 @@ angular.module('confusionApp').controller('MenuController',['$scope','menuFactor
 
 .controller('DishDetailController', ['$scope','$stateParams','menuFactory', function($scope,$stateParams,menuFactory) {
 
-  var dish = menuFactory.getDish(parseInt($stateParams.id,10));
-  $scope.dish = dish;
+  $scope.dish = {};
+  menuFactory.getDish(parseInt($stateParams.id,10)).then( function(response) {
+    $scope.dish = response.data;
+    $scope.showDish=true
+  });
 }])
 
 .controller('DishCommentController', ['$scope', function($scope) {
@@ -90,8 +96,11 @@ angular.module('confusionApp').controller('MenuController',['$scope','menuFactor
 // implement the IndexController and About Controller here
 .controller('IndexController', ['$scope','menuFactory','corporateFactory', function($scope,menuFactory,corporateFactory) {
 
-  var dish = menuFactory.getDish(0);
-  $scope.dish = dish;
+  $scope.dish = {};
+  menuFactory.getDish(0).then( function(response) {
+    $scope.dish = response.data;
+    $scope.showDish = true;
+  });
   var promotion = menuFactory.getPromotion(0);
   $scope.promotion = promotion;
   var leader = corporateFactory.getLeader(3);
